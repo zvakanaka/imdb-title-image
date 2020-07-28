e85de4b56eb962af94e0187bf19dbb854df2b0fe
Adam <8365885+zvakanaka@users.noreply.github.com>
2020-07-27 18:42:37 -0600

    add readme and write original title to filename

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ function cleanTitle(str) {
 }
 
 (async () => {
-  const title = process.argv.length > 2
-	? cleanTitle(process.argv[2])
-  : 'a beautiful day in the neighborhood';
+    if (process.argv.length < 3) {
+	console.log('Usage:\n\timdb-title-image "movie name"');
+    }
+    const originalTitle = process.argv[2];
+  const title = cleanTitle(process.argv[2]);
 
   const { document: findListDocument } = await window(`https://www.imdb.com/find?q=${encodeURIComponent(title)}&s=tt&ttype=ft&ref_=fn_ft`);
   const firstResult = findListDocument.querySelector('.findList a');
@@ -33,5 +35,5 @@ function cleanTitle(str) {
   const titleImgSrc = document.querySelector('.poster img').src;
   console.log(titleImgSrc);
 
-  request(titleImgSrc).pipe(fs.createWriteStream(`thumbs/${title}.jpg`));
+  request(titleImgSrc).pipe(fs.createWriteStream(`thumbs/${originalTitle}.jpg`));
 })();
