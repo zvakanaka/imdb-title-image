f8b16e129d67797a18a5fdf8a8b3c9df7819d81a
Adam Quinton <quintonish@gmail.com>
2020-07-27 18:30:59 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const { window } = require('page-evaluate');
+const request = require('request');
+const fs = require('fs');
+
+function strBeforeDot(str) {
+    const regexArr = str.match(/(.*)\./);
+    if (regexArr) {
+      return regexArr[1];
+    }
+    return str;
+}
+
+function cleanTitle(str) {
+    const beforeDotStr = strBeforeDot(str);
+    return beforeDotStr.toLowerCase().replace(/[_\-'"]/g, ' ');
+}
+
+(async () => {
+  const title = process.argv.length > 2
+	? cleanTitle(process.argv[2])
+  : 'a beautiful day in the neighborhood';
+
+  const { document: findListDocument } = await window(`https://www.imdb.com/find?q=${encodeURIComponent(title)}&s=tt&ttype=ft&ref_=fn_ft`);
+  const firstResult = findListDocument.querySelector('.findList a');
+  if (firstResult === null) {
+    throw new Error(`Could not find result for ${title}`)
+  }
+  const resultPageUrl = `https://www.imdb.com${firstResult.href}`;
+  console.log(resultPageUrl);
+  const { document } = await window(resultPageUrl);
+  const titleImgSrc = document.querySelector('.poster img').src;
+  console.log(titleImgSrc);
+
+  request(titleImgSrc).pipe(fs.createWriteStream(`thumbs/${title}.jpg`));
+})();
